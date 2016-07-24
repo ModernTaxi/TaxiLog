c87f5f3c930dcb8ab20ee1ec21b551440680e00e
Steve Decker <steve.decker@fullstackpro.com>
2016-07-23 20:54:09 -0700

    Initial Checkin

--- TaxiLog/MainActivity.cs
+++ TaxiLog/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
@@ -24,7 +25,87 @@ namespace TaxiLog
 			// and attach an event to it
 			Button button = FindViewById<Button>(Resource.Id.MyButton);
 
-			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+			button.Click += delegate
+			{
+				var geoUri = Android.Net.Uri.Parse("google.navigation:q=Swiss+Hotel, +Sonoma+California");
+				var mapIntent = new Intent(Intent.ActionView, geoUri);
+				StartActivity(mapIntent);
+			};
+
+			Button button2 = FindViewById<Button>(Resource.Id.LodgeInn);
+
+			button2.Click += delegate
+			{
+				var geoUri = Android.Net.Uri.Parse("google.navigation:q=Lodge+Inn, +Sonoma+California");
+				var mapIntent = new Intent(Intent.ActionView, geoUri);
+				StartActivity(mapIntent);
+			};
+
+			Button button3 = FindViewById<Button>(Resource.Id.GirlAndFig);
+
+			button3.Click += delegate
+			{
+				var geoUri = Android.Net.Uri.Parse("google.navigation:q=Girl+And+The+Fig, +Sonoma+California");
+				var mapIntent = new Intent(Intent.ActionView, geoUri);
+				StartActivity(mapIntent);
+			};
+
+			Button button4 = FindViewById<Button>(Resource.Id.EasyStop);
+
+			button4.Click += delegate
+			{
+				NavToMap("google.navigation:q=Easy+Stop, +Sonoma+California");
+			};
+
+			Button button5 = FindViewById<Button>(Resource.Id.lbl1);
+
+			button5.Click += delegate
+			{
+				NavToMap("google.navigation:q=Marys+Pizza+on+12, +Sonoma+California");
+			};
+
+			Button button6 = FindViewById<Button>(Resource.Id.lbl2);
+
+			button6.Click += delegate
+			{
+				NavToMap("google.navigation:q=Marys+Pizza+on+spain, +Sonoma+California");
+			};
+
+			Button button7 = FindViewById<Button>(Resource.Id.lbl3);
+
+			button7.Click += delegate
+			{
+				NavToMap("google.navigation:q=Sonoma+Valley+Hospital, +Sonoma+California");
+			};
+
+			Button button8 = FindViewById<Button>(Resource.Id.lbl4);
+
+			button8.Click += delegate
+			{
+				NavToMap("google.navigation:q=Brookdale+Sonoma, +Sonoma+California");
+			};
+
+			Button button9 = FindViewById<Button>(Resource.Id.lbl5);
+
+			button9.Click += delegate
+			{
+				NavToMap("google.navigation:q=Sonoma+Hills+Retirement, +Sonoma+California");
+			};
+
+			Button button10 = FindViewById<Button>(Resource.Id.lbl6);
+
+			button10.Click += delegate
+			{
+				NavToMap("google.navigation:q=Sonoma+Point+Apartments, +Sonoma+California");
+			};
+
+		}
+
+		private void NavToMap(string loc)
+		{
+			var geoUri = Android.Net.Uri.Parse(loc);
+			var mapIntent = new Intent(Intent.ActionView, geoUri);
+			StartActivity(mapIntent);
 		}
 	}
 }
